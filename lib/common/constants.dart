c93e4945fb773342a861a30acea6e633daf17ee1
Muhammad Yousif <m.alshf3ee@gmail.com>
2023-03-05 13:37:50 +0300

    update base urls config

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,6 @@
-const imagesUrl = "http://172.16.60.66:8000/media/";
+const baseUrl = "http://192.168.1.104:8000/";
+const apiUrl = "${baseUrl}requests/";
+const imagesUrl = "${baseUrl}media/";
 const imagePlaceHolder = "image/image_placeholder.png";
 
 final estatesTypes = ['Land', 'Farm', 'House', 'Villa', 'Store', 'Office', 'Building', 'Apartment'];
--- lib/common/helpers/http_client.dart
+++ lib/common/helpers/http_client.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_application_3/common/constants.dart';
 
 final dio = Dio(BaseOptions(
-  baseUrl: "http://172.16.60.66:8000/requests/",
+  baseUrl: apiUrl,
   connectTimeout: const Duration(seconds: 10),
   receiveTimeout: const Duration(seconds: 10),
   headers: {HttpHeaders.userAgentHeader: 'dio', 'common-header': 'xx'},
