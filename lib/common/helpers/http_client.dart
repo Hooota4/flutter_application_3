6b936777a448e0a750b9dc98a3abfa573f00e2ca
Muhammad Yousif <m.alshf3ee@gmail.com>
2023-03-06 07:29:51 +0300

    working on http client

--- lib/common/helpers/http_client.dart
+++ lib/common/helpers/http_client.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter_application_3/auth/controller/auth.dart';
 import 'package:flutter_application_3/common/constants.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dio = Dio(BaseOptions(
   baseUrl: apiUrl,
@@ -9,3 +11,16 @@ final dio = Dio(BaseOptions(
   receiveTimeout: const Duration(seconds: 10),
   headers: {HttpHeaders.userAgentHeader: 'dio', 'common-header': 'xx'},
 ));
+
+final httpProvider = StateProvider<Dio>((ref) {
+  final auth = ref.watch(authControllerProvider.notifier);
+
+  return Dio(
+    BaseOptions(
+      baseUrl: apiUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+      headers: {HttpHeaders.userAgentHeader: 'dio', 'common-header': 'xx', 'Authorization': 'Token bccea46620d3dd1d238b0d45f9cd39e2cb607b22'},
+    ),
+  );
+});
